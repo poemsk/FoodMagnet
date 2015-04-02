c2d80c7eb489164fc01bf2eac6cfca3bb45f9e56
Poe Poe <poepoe88@gmail.com>
2015-04-02 10:12:37 +0800

    remove google map

--- app/src/main/java/com/poepoemyintswe/foodmagnet/ui/NearbyShopFragment.java
+++ app/src/main/java/com/poepoemyintswe/foodmagnet/ui/NearbyShopFragment.java
@@ -1,9 +1,21 @@
 package com.poepoemyintswe.foodmagnet.ui;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by poepoe on 2/4/15.
  */
 public class NearbyShopFragment extends Fragment {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
 }
