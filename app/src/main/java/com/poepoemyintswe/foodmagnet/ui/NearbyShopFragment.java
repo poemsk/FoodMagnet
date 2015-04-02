e6f5729f98d1017a589663e905f80b86bfb69267
Poe Poe <poepoe88@gmail.com>
2015-04-02 10:08:36 +0800

    remove paddingtop from toolbar

---
+++ app/src/main/java/com/poepoemyintswe/foodmagnet/ui/NearbyShopFragment.java
@@ -0,0 +1,9 @@
+package com.poepoemyintswe.foodmagnet.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by poepoe on 2/4/15.
+ */
+public class NearbyShopFragment extends Fragment {
+}
